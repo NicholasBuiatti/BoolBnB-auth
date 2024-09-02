837ed442d7915dec4ac39468d2ee3b8b256ba532
Dihep <dihep.demayo@gmail.com>
2024-09-02 17:49:40 +0200

    Traduzione di alcuni messaggi di errore.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
 
 									@error('email')
 										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
+											<strong> Credenziali errate </strong>
 										</span>
 									@enderror
 								</div>
@@ -35,7 +35,7 @@
 
 									@error('password')
 										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
+											<strong> Credenziali errate </strong>
 										</span>
 									@enderror
 								</div>
