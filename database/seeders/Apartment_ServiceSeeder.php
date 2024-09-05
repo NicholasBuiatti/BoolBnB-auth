1eda01e4c7e095826b981de9489f894dafde5c1a
Mftr96 <matteo.federici96@gmail.com>
2024-09-05 18:31:56 +0200

    seeder pivot table; added condition to pivot table

--- database/seeders/Apartment_ServiceSeeder.php
+++ database/seeders/Apartment_ServiceSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+Use Illuminate\Support\Facades\DB;
 
 class Apartment_ServiceSeeder extends Seeder
 {
@@ -12,8 +13,22 @@ class Apartment_ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        for($i=0;$i<20;$i++){
+        $data=[];
+        $combNum=[];
+        while(count($data)<100){
+            $apartment_id=rand(1,45);
+            $service_id=rand(1,10);
+            $concNum=$apartment_id . '_' . $service_id;
+            if(!in_array($concNum,$combNum)){
+
+            };
+            $data[]=['apartment_id'=>$apartment_id,
+                      'service_id'=>$service_id];
+            $combArray[]=$concNum;
+            // $data->save();
+                    
+        };
             
-        }
+        DB::table('apartment_service')->insert($data);
     }
 }
