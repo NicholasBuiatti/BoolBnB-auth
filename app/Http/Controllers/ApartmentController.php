86b2afdcb79f1969caa8d13990b9e138ae96a5c7
NicholasBuiatti <nic.buiatti@gmail.com>
2024-09-07 19:24:44 +0200

    delete comm.

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
-
     // creo la funzione per trasformare l'indirizzo in coordinate tramite chiamata api
     public function getCoordinatesFromAddress($address)
     {
@@ -42,24 +41,17 @@ class ApartmentController extends Controller
             }
         }
     }
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-
         $user_id = Auth::id();
-        $catalogue = Apartment::where('user_id', $user_id)->with(['services'])->paginate(8);
         $data =
             [
-                'catalogue' => $catalogue,
+                'catalogue' => Apartment::where('user_id', $user_id)->with(['services'])->paginate(8),
             ];
         return view('admin.apartment.index', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
 
@@ -70,12 +62,8 @@ class ApartmentController extends Controller
         return view('admin.apartment.create', $data);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-
         //validating data inserted
         $data = $request->validate([
             "title" => "string|required|min:6",
@@ -95,7 +83,6 @@ class ApartmentController extends Controller
         //creating new istance of Apartment
         $newApartment = new Apartment();
 
-        $data['is_visible'] = $request->has('is_visible') ? 1 : 0;
 
         $newApartment->user_id = Auth::id();
         if ($request->has('image')) {
@@ -106,25 +93,22 @@ class ApartmentController extends Controller
         // salvo i dati delle coordinate nel database FUNZIONAAAAAAAA!
         $newApartment->longitude = $responseAddress['longitude'];
         $newApartment->latitude = $responseAddress['latitude'];
-        $newApartment->fill($data);
+        $data['is_visible'] = $request->has('is_visible') ? 1 : 0;
 
+        $newApartment->fill($data);
         $newApartment->save();
 
         if (isset($data['services'])) {
             $newApartment->services()->attach($data['services']);
         }
-        // dd($newApartment);
+
         return redirect()->route('apartments.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Apartment $apartment)
     {
         // Ottieni l'utente autenticato
         $user = auth()->user();
-
         // Verifica se l'utente autenticato è lo stesso dell'appartamento
         if ($apartment->user_id != $user->id) {
             // Se l'utente non è autorizzato, mostra la pagina 404
@@ -139,9 +123,6 @@ class ApartmentController extends Controller
         return view('admin.apartment.show', $data);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Apartment $apartment)
     {
         // Ottieni l'utente autenticato
@@ -152,19 +133,16 @@ class ApartmentController extends Controller
             // Se l'utente non è autorizzato, mostra la pagina 404
             abort(403);
         }
-        $service = Service::all();
+
         $data = [
             'apartment' => $apartment,
-            'services' => $service,
+            'services' => Service::all(),
             "relations" => $apartment->services->pluck('id')->toArray()
         ];
 
         return view('admin.apartment.edit', $data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Apartment $apartment)
     {
 
@@ -180,13 +158,7 @@ class ApartmentController extends Controller
             'services.*' => "exists:services,id",
 
         ]);
-        //$data=$request->all();
-        // $apartment->title = $data['title'];
-        // $apartment->rooms = $data['rooms'];
-        // $apartment->beds = $data['beds'];
-        // $apartment->bathrooms = $data['bathrooms'];
-        // $apartment->dimension_mq = $data['dimension_mq'];
-        // $apartment->image = $data['image'];
+
         if ($request->has('image')) {
             $img_path = Storage::put('uploads', $request->image);
             $data['image'] = $img_path;
@@ -198,12 +170,8 @@ class ApartmentController extends Controller
         $responseAddress = $this->getCoordinatesFromAddress($data['address_full']);
         $data['longitude'] = $responseAddress['longitude'];
         $data['latitude'] = $responseAddress['latitude'];
-        $data['is_visible'] = $request->has('is_visible') ? 1 : 0;
 
-        // $apartment->latitude = $data['latitude'];
-        // $apartment->longitude = $data['longitude'];
-        // $apartment->address_full = $data['address_full'];
-        // $apartment->is_visible=$data['is_visible'];
+        $data['is_visible'] = $request->has('is_visible') ? 1 : 0;
 
         $apartment->update($data);
 
@@ -214,9 +182,6 @@ class ApartmentController extends Controller
         return redirect()->route('apartments.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Apartment $apartment)
     {
 
