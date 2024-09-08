41d271e1ccf2689d32fa3459c39827e4125f7cde
NicholasBuiatti <nic.buiatti@gmail.com>
2024-09-08 13:16:14 +0200

    button modal for delete apartment

--- resources/views/admin/apartment/bin.blade.php
+++ resources/views/admin/apartment/bin.blade.php
@@ -10,11 +10,11 @@
 				<th scope="col">Letti</th>
 				<th scope="col">Mq</th>
 				<th scope="col">ripristina</th>
-                <th>elimina definitivamente</th>
+				<th>elimina definitivamente</th>
 			</tr>
 		</thead>
 		<tbody>
-			@foreach ($bin  as $apartment)
+			@foreach ($bin as $apartment)
 				<tr>
 					<th scope="row">{{ $apartment->title }}</th>
 					<td>{{ $apartment->rooms }}</td>
@@ -22,20 +22,48 @@
 					<td>{{ $apartment->beds }}</td>
 					<td>{{ $apartment->dimension_mq }}</td>
 					<td>
-                        <a class="btn btn-secondary" href="{{ route('apartments.restore', $apartment->id) }}">Restore</a>
+						<a class="btn btn-secondary" href="{{ route('apartments.restore', $apartment->id) }}">Restore</a>
 					</td>
-                    <td>
-                        <form class="px-5 pb-5" action="{{ route('apartments.forceDelete', $apartment->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button id="bottone" class="btn btn-danger" type="submit">elimina 
-                            </button>
-                        </form>
-                    </td>
+					<td>
+						{{-- BOTTONE CHE ATTIVA IL MODALE --}}
+						<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $apartment->id }}">
+							Cancella
+						</button>
+						{{-- MODALE DI BOOTSTRAP --}}
+						<div class="modal fade text-danger" id="modal-{{ $apartment->id }}" tabindex="-1" data-bs-backdrop="static"
+							data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $apartment->id }}" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="modalTitle-{{ $apartment->id }}">
+											Cancellazione
+										</h5>
+										<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+
+									<div class="modal-body">
+										Stai cancellando l'appartamento: {{ $apartment->title }}
+										<br>Sicuro di proseguire??
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+											Close
+										</button>
+
+										<form action="{{ route('apartments.destroy', $apartment) }}" method="POST">
+											@csrf
+											@method('DELETE')
+											<button class="btn btn-danger" type="submit">Elimina
+											</button>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</td>
 				</tr>
 			@endforeach
 		</tbody>
 	</table>
 	{{-- {{ $bin->links('pagination::bootstrap-5') }} --}}
-
 @endsection
--- resources/views/admin/apartment/show.blade.php
+++ resources/views/admin/apartment/show.blade.php
@@ -1,39 +1,69 @@
 @extends('layouts.navBar')
 
 @section('content')
-    <div class="d-flex align-items-start w-100">
+	<div class="d-flex align-items-start col">
 
-        <div class="d-flex flex-wrap">
-            <div id="card-container" class="card col">
-                <div class="img-container">
-                    @if (Str::startsWith($apartment->image, 'http'))
-                        <img width="140" class="card-img-top" src="{{ $apartment->image }}" alt="">
-                    @else
-                        <img width="140" class="card-img-top" src="{{ asset('storage/' . $apartment->image) }}"
-                            alt="">
-                    @endif
-                </div>
-                <div class="card-body">
-                    <h2 class="card-title">{{ $apartment->title }}</h2>
-                    <p class="card-text">
-                    <p><i class="fa-solid fa-person-shelter"></i> Stanze: {{ $apartment->rooms }}</p>
-                    <p><i class="fa-solid fa-bed"></i> Letti: {{ $apartment->beds }}</p>
-                    <p><i class="fa-solid fa-bath"></i> Bagni: {{ $apartment->bathrooms }}</p>
-                    <p><i class="fa-solid fa-maximize"></i> Superficie: {{ $apartment->dimension_mq }}mq</p>
-					<p><i class="fa-solid fa-house"></i> Indirizzo: {{$apartment->address_full}}</p>
-                    <div class="text-center">
-                        <a class="btn btn-primary my-2" href="{{ route('apartments.index') }}">Indietro</a>
-                        <a class="btn btn-secondary" href="{{ route('apartments.edit', $apartment->id) }}">Modifica</a>
-                        <form class="px-5 pb-5" action="{{ route('apartments.destroy', $apartment) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button id="bottone" class="btn btn-danger" type="submit">Elimina l'appartamento dalla tua lista
-                            </button>
-                        </form>
-                    </div>
+		<div class="d-flex flex-wrap">
+			<div id="card-container" class="card col">
+				<div class="img-container">
+					@if (Str::startsWith($apartment->image, 'http'))
+						<img width="140" class="card-img-top" src="{{ $apartment->image }}" alt="">
+					@else
+						<img width="140" class="card-img-top" src="{{ asset('storage/' . $apartment->image) }}" alt="">
+					@endif
+				</div>
+				<div class="card-body">
+					<h2 class="card-title">{{ $apartment->title }}</h2>
+					<p class="card-text">
+					<p><i class="fa-solid fa-person-shelter"></i> Stanze: {{ $apartment->rooms }}</p>
+					<p><i class="fa-solid fa-bed"></i> Letti: {{ $apartment->beds }}</p>
+					<p><i class="fa-solid fa-bath"></i> Bagni: {{ $apartment->bathrooms }}</p>
+					<p><i class="fa-solid fa-maximize"></i> Superficie: {{ $apartment->dimension_mq }}mq</p>
+					<p><i class="fa-solid fa-house"></i> Indirizzo: {{ $apartment->address_full }}</p>
+					<div class="text-center">
+						<a class="btn btn-primary my-2" href="{{ route('apartments.index') }}">Indietro</a>
+						<a class="btn btn-secondary" href="{{ route('apartments.edit', $apartment->id) }}">Modifica</a>
 
-                </div>
-            </div>
-        </div>
-    </div>
+						{{-- BOTTONE CHE ATTIVA IL MODALE --}}
+						<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $apartment->id }}">
+							Cancella
+						</button>
+						{{-- MODALE DI BOOTSTRAP --}}
+						<div class="modal fade text-danger" id="modal-{{ $apartment->id }}" tabindex="-1" data-bs-backdrop="static"
+							data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $apartment->id }}" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="modalTitle-{{ $apartment->id }}">
+											Cancellazione
+										</h5>
+										<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+
+									<div class="modal-body">
+										Stai cancellando l'appartamento: {{ $apartment->title }}
+										<br>Sicuro di proseguire??
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+											Close
+										</button>
+
+										<form action="{{ route('apartments.destroy', $apartment) }}" method="POST">
+											@csrf
+											@method('DELETE')
+											<button class="btn btn-danger" type="submit">Elimina
+											</button>
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+	</div>
 @endsection
