ba06fe0cf6ec2429666e73d98b0d5ac8dfcb5fcc
giacomo-spadoni <giaky.spado@gmail.com>
2024-09-09 16:43:47 +0200

    minor fixes and new photos

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -19,8 +19,8 @@ class ApartmentSeeder extends Seeder
         // Base coordinates
         $milanBaseLatitude = 45.4732;
         $milanBaseLongitude = 9.1895; // Adjusted to be more accurate for Milano
-        $parmaBaseLatitude=44.801485;
-        $parmaBaseLongitude=10.3279036;
+        $parmaBaseLatitude = 44.801485;
+        $parmaBaseLongitude = 10.3279036;
 
         // Function to generate random coordinates within a certain radius
         function generateRandomCoordinates($lat, $lon, $radiusKm)
@@ -106,7 +106,69 @@ class ApartmentSeeder extends Seeder
         ];
 
 
-        //ciclo appartamenti Milano 
+        // //ciclo appartamenti Milano
+        // for ($i = 0; $i < 30; $i++) {
+        //     $newApartment = new Apartment();
+        //     $newApartment->user_id = rand(1, 3);
+        //     $newApartment->title = "Villa " . $milanStreets[$i];
+        //     $newApartment->rooms = rand(1, 5);
+        //     $newApartment->beds = rand(1, 4);
+        //     $newApartment->bathrooms = rand(1, 2);
+        //     $newApartment->dimension_mq = rand(40, 120);
+        //     $newApartment->image = "http://www.case-in-legno-progettolegno.it/wp-content/uploads/2015/12/Casa-in-legno-a-Fano-0003.jpg";
+
+        //     list($newApartment->latitude, $newApartment->longitude) = generateRandomCoordinates($milanBaseLatitude, $milanBaseLongitude, 40);
+
+        //     $newApartment->address_full = $milanStreets[$i] . ", " . rand(1, 100) . ", 20100, Milano";
+        //     $newApartment->is_visible = true;
+        //     $newApartment->save();
+        // }
+        // //ciclo appartamenti Parma
+        // for ($i = 0; $i < 15; $i++) {
+        //     $newApartment = new Apartment();
+        //     $newApartment->user_id = rand(1, 3);
+        //     $newApartment->title = "Villa " . $parmaStreets[$i];
+        //     $newApartment->rooms = rand(1, 5);
+        //     $newApartment->beds = rand(1, 4);
+        //     $newApartment->bathrooms = rand(1, 2);
+        //     $newApartment->dimension_mq = rand(40, 120);
+        //     $newApartment->image = "http://www.case-in-legno-progettolegno.it/wp-content/uploads/2015/12/Casa-in-legno-a-Fano-0003.jpg";
+
+        //     list($newApartment->latitude, $newApartment->longitude) = generateRandomCoordinates($parmaBaseLatitude, $parmaBaseLongitude, 40);
+
+        //     $newApartment->address_full = $parmaStreets[$i] . ", " . rand(1, 100) . ", 43125, Parma";
+        //     $newApartment->is_visible = true;
+        //     $newApartment->save();
+        // }
+
+        // Aggiungi un array di immagini casuali
+        $imageUrls = [
+            "https://plus.unsplash.com/premium_photo-1684175656320-5c3f701c082c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1499916078039-922301b0eb9b?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1499955085172-a104c9463ece?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1674676471104-3c4017645e6f?q=80&w=1940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1527772482340-7895c3f2b3f7?q=80&w=2151&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1496180727794-817822f65950?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1683769250375-1bdf0ec9d80f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1515263487990-61b07816b324?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1484154218962-a197022b5858?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1683133660598-3ebeb26769ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1500307353842-81f527a10685?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1495433324511-bf8e92934d90?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1504624720567-64a41aa25d70?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1520106392146-ef585c111254?q=80&w=2025&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1674676471963-4c4643beb12d?q=80&w=1939&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1683140513388-4344c8fc2778?q=80&w=2014&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1485819196298-11222a657b31?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1501877008226-4fca48ee50c1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1509660933844-6910e12765a0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1528255671579-01b9e182ed1d?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1674676471447-b893f7aefbd4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        ];
+
+        // ciclo appartamenti Milano
         for ($i = 0; $i < 30; $i++) {
             $newApartment = new Apartment();
             $newApartment->user_id = rand(1, 3);
@@ -115,15 +177,17 @@ class ApartmentSeeder extends Seeder
             $newApartment->beds = rand(1, 4);
             $newApartment->bathrooms = rand(1, 2);
             $newApartment->dimension_mq = rand(40, 120);
-            $newApartment->image = "http://www.case-in-legno-progettolegno.it/wp-content/uploads/2015/12/Casa-in-legno-a-Fano-0003.jpg";
+
+            // Assegna un'immagine casuale
+            $newApartment->image = $imageUrls[rand(0, count($imageUrls) - 1)];
 
             list($newApartment->latitude, $newApartment->longitude) = generateRandomCoordinates($milanBaseLatitude, $milanBaseLongitude, 40);
-
             $newApartment->address_full = $milanStreets[$i] . ", " . rand(1, 100) . ", 20100, Milano";
             $newApartment->is_visible = true;
             $newApartment->save();
         }
-        //ciclo appartamenti Parma
+
+        // ciclo appartamenti Parma
         for ($i = 0; $i < 15; $i++) {
             $newApartment = new Apartment();
             $newApartment->user_id = rand(1, 3);
@@ -132,16 +196,14 @@ class ApartmentSeeder extends Seeder
             $newApartment->beds = rand(1, 4);
             $newApartment->bathrooms = rand(1, 2);
             $newApartment->dimension_mq = rand(40, 120);
-            $newApartment->image = "http://www.case-in-legno-progettolegno.it/wp-content/uploads/2015/12/Casa-in-legno-a-Fano-0003.jpg";
+
+            // Assegna un'immagine casuale
+            $newApartment->image = $imageUrls[rand(0, count($imageUrls) - 1)];
 
             list($newApartment->latitude, $newApartment->longitude) = generateRandomCoordinates($parmaBaseLatitude, $parmaBaseLongitude, 40);
-
             $newApartment->address_full = $parmaStreets[$i] . ", " . rand(1, 100) . ", 43125, Parma";
             $newApartment->is_visible = true;
             $newApartment->save();
         }
-
     }
-
-
 }
--- database/seeders/Apartment_ServiceSeeder.php
+++ database/seeders/Apartment_ServiceSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-Use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 
 class Apartment_ServiceSeeder extends Seeder
 {
@@ -13,22 +13,26 @@ class Apartment_ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        $data=[];
-        $combNum=[];
-        while(count($data)<100){
-            $apartment_id=rand(1,45);
-            $service_id=rand(1,10);
-            $concNum=$apartment_id . '_' . $service_id;
-            if(!in_array($concNum,$combNum)){
-
+        $data = [];
+        $combNum = [];
+        while (count($data) < 200) {
+            $apartment_id = rand(1, 45);
+            $service_id = rand(1, 10);
+            $concNum = $apartment_id . '_' . $service_id;
+            if (!in_array($concNum, $combNum)) {
+                $data[] = [
+                    'apartment_id' => $apartment_id,
+                    'service_id' => $service_id
+                ];
+                $combNum[] = $concNum;
             };
-            $data[]=['apartment_id'=>$apartment_id,
-                      'service_id'=>$service_id];
-            $combArray[]=$concNum;
+            // $data[]=['apartment_id'=>$apartment_id,
+            //           'service_id'=>$service_id];
+            // $combArray[] = $concNum;
             // $data->save();
-                    
+
         };
-            
+
         DB::table('apartment_service')->insert($data);
     }
 }
