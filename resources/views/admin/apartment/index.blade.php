6c87660758ecee110b80913c3ede171adf424dce
NicholasBuiatti <nic.buiatti@gmail.com>
2024-09-11 22:44:56 +0200

    fix del fix del fix

--- resources/views/admin/apartment/index.blade.php
+++ resources/views/admin/apartment/index.blade.php
@@ -16,16 +16,16 @@
 			@foreach ($catalogue as $apartment)
 				<tr class="text-center align-middle" style="background-color: #ffffff; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
 
-					<th scope="row" class="d-block d-sm-table-cell col-12">
+					<th scope="row" class="d-block d-sm-table-cell col-12 col-md-5">
 						<img class="rounded shadow-sm" src="{{ $apartment->image }}" alt="Immagine appartamento"
-							style="height: 8rem; width:10rem; object-fit: cover;">
+							style="height: 8rem; width:15rem; object-fit: cover;">
 					</th>
 					<td class="d-block d-sm-table-cell col-12 col-md-6">
 						<h3 class="fs-4 title-shadow">{{ $apartment->title }}</h3>
 					</td>
 
 
-					<td class="d-block d-sm-table-cell col-12 col-md-6">
+					<td class="d-block d-sm-table-cell col-12 col-md-3">
 						@if ($apartment->lastSponsorship)
 							<p class="fw-light">Fine della Sponsorizzazione: <br> {{ $apartment->lastSponsorship->pivot->ending_date }}</p>
 						@endif
@@ -55,7 +55,7 @@
 													{{ $sponsorship->name == 'Premium' ? 'background: linear-gradient(130deg, #f0f4f8, #c0c0c0);' : '' }}
 													 {{ $sponsorship->name == 'Elite'
 													    ? 'background: linear-gradient(130deg, #fff5e4, #f2c27f);
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																				'
+																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																				'
 													    : '' }}">
 													<div class="card-header text-uppercase fs-3 fw-bold">{{ $sponsorship->name }}</div>
 													<div class="card-body">
