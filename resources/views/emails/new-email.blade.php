f882d649ba93a33809040055dee1896a9cb86418
Mftr96 <matteo.federici96@gmail.com>
2024-09-11 23:02:33 +0200

    working on mail 2

---
+++ resources/views/emails/new-email.blade.php
@@ -0,0 +1,12 @@
+{{-- mail di prova --}}
+
+@dd($message)
+{{-- <h1>Ciao!</h1>
+<p>hai ricevuto un nuovo messaggio, e questi sono i dettagli :</p>
+<p>nome:{{$message->name}}</p>
+<p>email:{{$message->email}}</p>
+<p>oggetto mail:{{$message->text}}</p>
+<p>relativo all'appartamento:{{$message->apartment->title}}</p>
+<br>
+<p>cordiali saluti </p>
+ --}}
