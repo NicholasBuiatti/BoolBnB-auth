94d8712551b06a2d3fe1e42255ec0b416c13fb29
NicholasBuiatti <nic.buiatti@gmail.com>
2024-09-12 12:05:06 +0200

    massage in dashboard

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SponsorshipController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Apartment;
+use App\Models\Message;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -28,9 +29,13 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     $user_id = Auth::id();
     $catalogue = Apartment::where('user_id', $user_id)->with(['services'])->paginate(8);
+    $messages = Message::whereHas('apartment', function ($query) use ($user_id) {
+        $query->where('user_id', $user_id);
+    })->paginate(10);
     $data =
         [
             'catalogue' => $catalogue,
+            'messages' => $messages,
         ];
     return view('dashboard', $data);
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -40,16 +45,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::delete('/apartments/force-delete/{id}', [ApartmentController::class, 'forceDelete'])->name('apartments.forceDelete');
+
+    // ROTTE APARTAMENTI E SOFT DELETE
+
     Route::get('/apartments/bin', [ApartmentController::class, 'bin'])->name('apartments.bin');
     Route::resource('/apartments', ApartmentController::class);
     Route::get('/apartments/restore/{id}', [ApartmentController::class, 'restore'])->name('apartments.restore');
+
+    // ROTTE PER I MESSAGGI
+
     Route::get('/message', [MessageController::class, 'index'])->name('message.index');
-    Route::post('/message/store', [MessageController::class, 'store'])->name('message.store');
     Route::get('/message/{message}', [MessageController::class, 'show'])->name('message.show');
+    Route::post('/message/store', [MessageController::class, 'store'])->name('message.store');
+
+    // ROTTE PER LA SPONSORSHIP
 
     Route::get('/success/{id}', [SponsorshipController::class, 'success'])->name('success');
-
-
     Route::get('/apartments/{apartment}/sponsorships', [ApartmentController::class, 'showSponsorships'])->name('sponsorships.index');
     Route::post('/apartments/{apartment}/sponsorships/payment', [SponsorshipController::class, 'processPayment'])->name('sponsorships.payment');
 });
