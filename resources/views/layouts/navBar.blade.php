4c056cbe02e7492dd0e1a9a170cab3d52f9c17b5
giacomo-spadoni <giaky.spado@gmail.com>
2024-09-13 00:28:45 +0200

    FIXFIXFIX ICOOOONA

--- resources/views/layouts/navBar.blade.php
+++ resources/views/layouts/navBar.blade.php
@@ -10,6 +10,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="icon" type="image/svg+xml" href="/logo.png" />
+
     {{-- Font-Awesome --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
